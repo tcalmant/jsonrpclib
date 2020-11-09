fec8ea66014e70faebb735ea0a161f05d25922fa
Thomas Calmant <thomas.calmant@gmail.com>
2020-11-09 21:53:00 +0100

    Fixed version number in doc

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Thomas Calmant'
 # The short X.Y version
 version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '0.4.2'
 
 
 # -- General configuration ---------------------------------------------------
