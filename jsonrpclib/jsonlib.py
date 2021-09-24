c210811238e36db79be21a4eb7b52af5d7b2db1a
Thomas Calmant <thomas.calmant@gmail.com>
2021-09-24 22:25:34 +0200

    Added license header to jsonlib

--- jsonrpclib/jsonlib.py
+++ jsonrpclib/jsonlib.py
@@ -3,13 +3,46 @@
 """
 Loads the "best" Python library available for the current interpreter and
 provides a single interface for all
+
+:authors: Thomas Calmant
+:copyright: Copyright 2021, Thomas Calmant
+:license: Apache License 2.0
+:version: 0.4.3
+
+..
+
+    Copyright 2021 Thomas Calmant
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 
+# Standard library
 import json
 import sys
 
+# ------------------------------------------------------------------------------
 
+# Module version
+__version_info__ = (0, 4, 3)
+__version__ = ".".join(str(x) for x in __version_info__)
+
+# Documentation strings format
+__docformat__ = "restructuredtext en"
+
+# Python version flag
 PYTHON_2 = sys.version_info[0] < 3
+
+# ------------------------------------------------------------------------------
 
 
 class JsonHandler(object):
