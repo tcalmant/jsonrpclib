a9ac10c6ea41d66fe64c41637ee3c14bb54c5f09
Thomas Calmant <thomas.calmant@gmail.com>
2022-02-19 17:14:03 +0100

    Replaced nose by pytest

--- run_test.sh
+++ run_test.sh
@@ -4,30 +4,34 @@
 #
 
 echo "Installing dependencies..."
-pip install nose coverage || exit 1
+pip install pytest coverage || exit 1
 export COVERAGE_PROCESS_START=".coveragerc"
 
 echo "Initial tests..."
 export JSONRPCLIB_TEST_EXPECTED_LIB=json
-nosetests -v --exe --with-coverage --cover-package=jsonrpclib tests || exit 1
+coverage run -m pytest || exit 1
 
 echo "uJson tests..."
 pip install ujson && (
     export JSONRPCLIB_TEST_EXPECTED_LIB=ujson
-    nosetests -v --with-coverage --cover-package=jsonrpclib tests/test_jsonlib.py || exit 1
+    coverage run -m pytest tests/test_jsonlib.py || exit 1
     pip uninstall -y ujson
 )
 
 echo "cJson tests..."
 pip install python-cjson && (
     export JSONRPCLIB_TEST_EXPECTED_LIB=cjson
-    nosetests -v --with-coverage --cover-package=jsonrpclib tests/test_jsonlib.py || exit 1
+    coverage run -m pytest tests/test_jsonlib.py || exit 1
     pip uninstall -y python-cjson
 )
 
 echo "simplejson tests..."
 pip install simplejson && (
     export JSONRPCLIB_TEST_EXPECTED_LIB=simplejson
-    nosetests -v --with-coverage --cover-package=jsonrpclib tests/test_jsonlib.py || exit 1
+    coverage run -m pytest tests/test_jsonlib.py || exit 1
     pip uninstall -y simplejson
 )
+
+echo "Combine results..."
+coverage combine || exit $?
+coverage report
